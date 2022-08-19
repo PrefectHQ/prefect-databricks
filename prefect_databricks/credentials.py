9209f656f1be3208d9b460cf11032a837ae40c2b
ahuang11 <andrew.h@prefect.io>
2022-08-19 14:12:05 -0700

    Fix credentials args to attributes

--- prefect_databricks/credentials.py
+++ prefect_databricks/credentials.py
@@ -11,7 +11,7 @@ class DatabricksCredentials(Block):
     """
     Block used to manage Databricks authentication.
 
-    Args:
+    Attributes:
         databricks_instance:
             Databricks instance used in formatting the endpoint URL.
         token: the token to authenticate with Databricks.
