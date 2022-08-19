d2ff03c3c7242a028d84a4d16123fce2182ce858
ahuang11 <andrew.h@prefect.io>
2022-08-19 14:51:20 -0700

    Linting

--- prefect_databricks/models/jobs.py
+++ prefect_databricks/models/jobs.py
@@ -5,8 +5,8 @@
 from __future__ import annotations
 
 from enum import Enum
-
 from typing import Any, Dict, List, Optional, Union
+
 from pydantic import BaseModel, Extra, Field, constr
 from typing_extensions import Literal
 
