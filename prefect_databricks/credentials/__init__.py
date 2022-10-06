b5b99056769ba5e5f2f7139e9f0039fda5b83b57
ahuang11 <andrew.h@prefect.io>
2022-10-05 17:32:04 -0700

    Override credentials

--- prefect_databricks/credentials/__init__.py
+++ prefect_databricks/credentials/__init__.py
@@ -1,1 +1,6 @@
-from ._generated import *  # noqa: F401, F403
+from ._generated import _DatabricksCredentials
+
+class DatabricksCredentials(_DatabricksCredentials):
+
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/5GTHI1PH2dTiantfps6Fnc/1c750fab7f4c14ea1b93a62b9fea6a94/databricks_logo_icon_170295.png?h=250" # noqa
+
