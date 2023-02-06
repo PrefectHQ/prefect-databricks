8ce5e3fca8d49a0a1f3cff22061b4aefb3fd7349
nate nowack <nate@prefect.io>
2023-02-06 12:22:54 -0600

    add flow to `__init__`

--- prefect_databricks/__init__.py
+++ prefect_databricks/__init__.py
@@ -1,4 +1,5 @@
 from . import _version
 from .credentials import DatabricksCredentials  # noqa
+from .flows import jobs_runs_submit_and_wait_for_completion # noqa
 
 __version__ = _version.get_versions()["version"]
